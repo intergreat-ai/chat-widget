bdda9302a637588e36e1c589c31d7691c3417328
Jay Sync <63868998+jay-sync@users.noreply.github.com>
2025-06-12 19:13:55 -0600

    Update chat-widget-x.js

--- chat-widget-x.js
+++ chat-widget-x.js
@@ -347,7 +347,6 @@
             <input id="user-first-name" placeholder="First Name" />
             <input id="user-last-name" placeholder="Last Name" />
             <input id="user-email" type="email" placeholder="Email" />
-            <p class="response-text">${config.branding.responseTimeText}</p>
             <button class="new-chat-btn" id="start-chat-btn">
                 <svg class="message-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                     <path fill="currentColor" d="M20 2H4c-1.1 0-2 .9-2 2v18l4-4h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm0 14H5.2L4 17.2V4h16v12z"/>
